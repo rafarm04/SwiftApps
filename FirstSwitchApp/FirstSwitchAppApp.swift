da0c6f51a1b258b64187b35065bd7717a87eca76
rafarm04 <wafar0mo@hotmail.com>
2024-11-29 17:57:26 -0600

    Initial Commit

---
+++ FirstSwitchApp/FirstSwitchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwitchAppApp.swift
+//  FirstSwitchApp
+//
+//  Created by Rafael Romo Muñoz on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwitchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstSwitchAppTests/FirstSwitchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwitchAppTests.swift
+//  FirstSwitchAppTests
+//
+//  Created by Rafael Romo Muñoz on 29/11/24.
+//
+
+import Testing
+@testable import FirstSwitchApp
+
+struct FirstSwitchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
